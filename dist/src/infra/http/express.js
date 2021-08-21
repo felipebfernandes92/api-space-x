acdc7d54d08cdda5cbbc9c825fe1914fe8c976c3
Felipe Fernandes <felipe.fernandes@gam.com.br>
2021-08-21 18:58:45 -0300

    build: :building_construction: Updated folder structure.

--- dist/src/infra/http/express.js
+++ dist/src/infra/http/express.js
@@ -7,8 +7,5 @@ const ExpressAdapter_1 = __importDefault(require("../../adapter/ExpressAdapter")
 const LaunchController_1 = __importDefault(require("../../controller/LaunchController"));
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-const port = 8080;
 app.get('/', ExpressAdapter_1.default.create(LaunchController_1.default.getNextLaunch));
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+app.listen(process.env.PORT || 8080);
