78cbf09f050e6dc8b43d973870aaa204caf004e3
Felipe Fernandes <felipe.fernandes@gam.com.br>
2021-08-21 18:48:43 -0300

    build: :hammer: Adjustment to Heroku.

--- api/src/infra/http/express.ts
+++ api/src/infra/http/express.ts
@@ -2,7 +2,6 @@ import ExpressAdapter from "../../adapter/ExpressAdapter";
 import LaunchController from "../../controller/LaunchController";
 import express from 'express';
 const app = express();
-const port = 8080;
 
 app.get('/', ExpressAdapter.create(LaunchController.getNextLaunch));
 
