a5d440e3728564d098496aeeb455a56a0c9bb46b
Felipe Fernandes <felipe.fernandes@gam.com.br>
2021-08-21 18:40:06 -0300

    build: :hammer: Adjustment to Heroku.

--- api/src/infra/http/express.ts
+++ api/src/infra/http/express.ts
@@ -4,9 +4,6 @@ import express from 'express';
 const app = express();
 const port = 8080;
 
-
 app.get('/', ExpressAdapter.create(LaunchController.getNextLaunch));
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+app.listen(process.env.PORT || 8080);
